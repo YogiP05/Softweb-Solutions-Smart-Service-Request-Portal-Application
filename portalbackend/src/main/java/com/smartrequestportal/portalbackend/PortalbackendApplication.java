e7b768a5d43ac506f833104dc0af032f9535e401
YogiP05 <yo0510gi@gmail.com>
2025-05-29 20:33:01 -0400

    completed spring boot/maven setup

---
+++ portalbackend/src/main/java/com/smartrequestportal/portalbackend/PortalbackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartrequestportal.portalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalbackendApplication.class, args);
+	}
+
+}
