3356d136caf432195e1a0cdc57ed2a013373791e
YogiP05 <yo0510gi@gmail.com>
2025-06-04 10:26:49 -0500

    mock user class and home controller

--- portalbackend/src/main/java/com/smartrequestportal/portalbackend/PortalbackendApplication.java
+++ portalbackend/src/main/java/com/smartrequestportal/portalbackend/PortalbackendApplication.java
@@ -1,12 +1,14 @@
 package com.smartrequestportal.portalbackend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortalbackendApplication {
 
-	public static void main(String[] args) {
+	public static <logger> void main(String[] args) {
 		SpringApplication.run(PortalbackendApplication.class, args);
 	}
 
